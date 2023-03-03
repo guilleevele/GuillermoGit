0bc26d318888c80107f64cf23631fd9d30cacaa6
guill <guill@DESKTOP-RR8G6VJ>
2023-03-03 17:17:44 +0100

    Cambio para el conflicto

--- src/guillermo/git/GuillermoGIT.java
+++ src/guillermo/git/GuillermoGIT.java
@@ -7,6 +7,8 @@ public class GuillermoGIT {
     // Guillermo Torregrosa López
     
         System.out.println("Hola, soy Guillermo Torregrosa");
+        System.out.println("Tengo 27 años");
+        System.out.println("Vivo en Pulpí, Almería");
         
     }
     
