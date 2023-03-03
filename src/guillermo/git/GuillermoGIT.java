81d3118624fca5603cd2b7ca87eb830e5ec70088
guill <guill@DESKTOP-RR8G6VJ>
2023-03-03 16:42:34 +0100

    Commit primer cambio

--- src/guillermo/git/GuillermoGIT.java
+++ src/guillermo/git/GuillermoGIT.java
@@ -4,7 +4,9 @@ package guillermo.git;
 public class GuillermoGIT {
 
     public static void main(String[] args) {
+    // Guillermo Torregrosa López
     
+        System.out.println("Hola, soy Guillermo Torregrosa");
         
     }
     
